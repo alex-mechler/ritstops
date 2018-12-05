4eab09314ddaad7cff555e6904054e98d8dbcc4e
Alexadner Mechler <alex@alexwritescode.com>
2018-12-05 17:12:39 -0500

    Standardize

--- api/routes/research.js
+++ api/routes/research.js
@@ -7,27 +7,28 @@ module.exports = router;
 
 router.get('/', async(req, res) =>{
 	const {rows} = await db.query('SELECT stop.name, stop.loc, quest.reward, quest.quest, stop.confirmed FROM stop JOIN quest ON stop.quest_id = quest.id');
-  res.send(rows);
+  res.send({err: false, message: '', result: rows});
 });
 
 router.post('/', async(req, res) => {
 	const stop = req.body.stop;
 	const quest = req.body.quest;
 	var {rows} = await db.query('SELECT COUNT(id) FROM stop WHERE id = $1', [stop]);
-	if(rows[0]['count'] != 1){
-		res.send({err: true, message: 'Invalid Stop ID'});
+	if(rows[0]['count'] != 1) {
+		res.send({err: true, message: 'Invalid Stop ID', result: {}});
 		return;
 	}
 	var {rows} = await db.query('SELECT COUNT(id) FROM quest WHERE id = $1', [quest]);
-	if(rows[0]['count'] != 1){
-		res.send({err: true, message: 'Invalid Quest ID'});
+	if(rows[0]['count'] != 1) {
+		res.send({err: true, message: 'Invalid Quest ID', result: {}});
 		return;
 	}
 	var {rows} = await db.query('SELECT quest_id FROM stop WHERE id = $1', [stop]);
-	if(rows[0]['quest_id'] != 1){
-		res.send({err: true, message: 'Stop has already been reported'});
+	if(Number(rows[0]['quest_id']) != 1) {
+		res.send({err: true, message: 'Stop has already been reported', result: {}});
 		return;
 	}
-	await db.query('UPDATE stop SET quest_id = $1 WHERE id = $1', [quest]);
-	res.send({err: false, message: ''});
+	await db.query('UPDATE stop SET quest_id = $1 WHERE id = $2', [quest, stop]);
+	var {rows} = await db.query('SELECT stop.name, stop.loc, quest.reward, quest.quest, stop.confirmed FROM stop JOIN quest ON stop.quest_id = quest.id WHERE stop.id = $1', [stop]);
+	res.send({err: false, message: '', result: rows});
 });
--- api/routes/stop.js
+++ api/routes/stop.js
@@ -7,15 +7,15 @@ module.exports = router;
 
 router.get('/', async(req, res) => {
 	const {rows} = await db.query('SELECT id, name, loc FROM stop');
-	res.send(rows);
+	res.send({err: false, message: '', result: rows});
 });
 
 router.get('/:id', async(req, res) => {
 	const {rows} = await db.query('SELECT id, name, loc FROM stop WHERE id = $1', [req.params.id]);
-	if(rows.length == 1){
-		res.send(rows[0]);
-	}else{
-		res.send({err: true, message: 'No such stop'});
+	if(rows.length == 1) {
+		res.send({err: true, message: '', result: rows});
+	} else {
+		res.send({err: true, message: 'No such stop', result: {}});
 	}
 });
 
@@ -24,24 +24,24 @@ router.post('/', async(req, res) => {
 	const lat = Number(req.body.lat);
 	const long = Number(req.body.long);
 	console.log(long);
-	if(name == undefined){
-		res.send({err: true, message: 'Invalid name'});
+	if(name == undefined) {
+		res.send({err: true, message: 'Invalid name', result: {}});
 		return;
 	}
 	if(lat == undefined || isNaN(lat)) {
-		res.send({err: true, message: 'Invalid lat'});
+		res.send({err: true, message: 'Invalid lat', result: {}});
 		return;
 	}
 	if(long == undefined || isNaN(long)) {
-		res.send({err: true, message: 'Invalid long'});
+		res.send({err: true, message: 'Invalid long', result: {}});
 		return;
 	}
 	var {rows} = await db.query('SELECT COUNT(id) FROM stop WHERE name = $1', [name]);
-	if(rows[0].count != 0){
-		res.send({err: true, message: 'A stop of that name already exists!'});
+	if(rows[0].count != 0) {
+		res.send({err: true, message: 'A stop of that name already exists', result: {}});
 		return;
 	}
 	await db.query('INSERT INTO stop (name, loc) VALUES ($1, point($2, $3))', [name, lat, long]);
 	var {rows} = await db.query('SELECT id, name, loc FROM stop WHERE name = $1', [name]);
-	res.send({err: false, message: '', result: rows[0]});
+	res.send({err: false, message: '', result: rows});
 });
