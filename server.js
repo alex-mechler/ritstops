f9710b67dc84ede6028a64adfddd52867b090ed4
Alexadner Mechler <alex@alexwritescode.com>
2018-12-05 12:13:47 -0500

    /api/research routes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 const research = require('./api/routes/research');
 app.use('/api/research', research);
 
