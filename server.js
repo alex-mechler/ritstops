3ac4087bafa5da0913e07fe7b23e742565e2b80a
Alexadner Mechler <alex@alexwritescode.com>
2018-12-06 17:09:21 -0500

    Discord auth

--- server.js
+++ server.js
@@ -3,7 +3,38 @@ require('dotenv').config();
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
+var session = require('express-session');
+var passport = require('passport');
+var DiscordStrategy = require('passport-discord').Strategy;
 
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+var scopes = ['identify', 'email', 'guilds'];
+
+passport.use(new DiscordStrategy({
+	clientID: process.env.DISCORD_CLIENT_ID,
+	clientSecret: process.env.DISCORD_CLIENT_SECRET,
+	callbackURL: process.env.DISCORD_CALLBACK_URL,
+	scope: scopes
+}, function(accessToken, refreshToken, profile, done) {
+	process.nextTick(function() {
+		return done(null, profile);
+	})
+}));
+
+app.use(session({
+	secret: process.env.COOKIE_SECRET,
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -15,6 +46,20 @@ app.use('/api/stop', stop);
 
 const quest = require('./api/routes/quest');
 app.use('/api/quest', quest);
+
+app.get('/api/auth/login', passport.authenticate('discord', {scope: scopes}), function(req, res) {});
+app.get('/api/auth/callback', 
+	passport.authenticate('discord', {failureRedirect: '/'}),
+	function(req, res) {res.redirect('/')}
+);
+app.get('/api/auth/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+app.get('/', function(req, res) {
+	res.send(req.user);
+})
 
 app.listen(port);
 
