015bafdb6ba2db69b8f086cbc7bc4b72515dadd3
Alexadner Mechler <alex@alexwritescode.com>
2019-01-13 11:49:15 -0500

    Fix icons

--- updateQuests.js
+++ updateQuests.js
@@ -339,12 +339,12 @@ quests = [
   {
     "reward": "Anorith",
     "quest": "Catch 7 Flying Psychic or Dark-type Pokemon",
-    "icon": "Psychic"
+    "icon": "icon_anorith"
   },
   {
     "reward": "Starmie",
     "quest": "Catch 5 Electric Normal or Posion-type Pokemon",
-    "icon": "Normal"
+    "icon": "icon_starmie"
   },
   {
     "reward": "Kabuto",
