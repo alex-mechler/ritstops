2ecb4ed3c36388154f61a6f332e574e1634c3df5
Alexadner Mechler <alex@alexwritescode.com>
2019-01-15 18:46:10 -0500

    Remove extra tables

--- createTables.js
+++ createTables.js
@@ -5,8 +5,6 @@ const db = require('./api/util/db.js');
 createTables();
 
 async function createTables(){
-	await db.query("CREATE TABLE quest (id SERIAL PRIMARY KEY, reward VARCHAR(50), quest VARCHAR(100), icon VARCHAR(100))");
-	await db.query("CREATE TABLE stop (id SERIAL PRIMARY KEY, name VARCHAR(50), loc POINT, quest_id INT DEFAULT 1, confirmed BOOLEAN DEFAULT FALSE, FOREIGN KEY (quest_id) REFERENCES quest(id))");
 	await db.query("CREATE TABLE session (sid varchar NOT NULL COLLATE \"default\", sess json NOT NULL, expire timestamp(6) NOT NULL) WITH (OIDS=FALSE)");
 	await db.query("ALTER TABLE session ADD CONSTRAINT \"session_pkey\" PRIMARY KEY (sid) NOT DEFERRABLE INITIALLY IMMEDIATE");
 	await db.query("CREATE TABLE discord_user (id SERIAL PRIMARY KEY, email VARCHAR(50) UNIQUE, username VARCHAR(50) NOT NULL, discord_id VARCHAR(32), api_key CHAR(32) NOT NULL, avatar CHAR(32), preferences JSONB, score INT DEFAULT 0, donation_amount INT DEFAULT 0)");
