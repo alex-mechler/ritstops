668f3d26433bd67dba41ad1bef837a5ad3a6e0a1
Alexander Mechler <alex@alexwritescode.com>
2019-02-01 09:15:11 -0500

    Filter out 0 score

--- api/routes/leaderboard.js
+++ api/routes/leaderboard.js
@@ -6,11 +6,11 @@ const router = new Router();
 module.exports = router;
 
 router.get('/', async(req, res) => {
-	const {rows} = await db.query('SELECT username, score FROM discord_user WHERE visible != FALSE ORDER BY score DESC');
+	const {rows} = await db.query('SELECT username, score FROM discord_user WHERE visible != FALSE AND score > 0 ORDER BY score DESC');
 	res.send({err: false, message: '', result: rows});
 });
 
 router.get('/seasonal', async(req, res) => {
-	const {rows} = await db.query('SELECT username, seasonal_score FROM discord_user WHERE visible != FALSE ORDER BY seasonal_score DESC');
+	const {rows} = await db.query('SELECT username, seasonal_score FROM discord_user WHERE visible != FALSE AND seasonal_score > 0 ORDER BY seasonal_score DESC');
 	res.send({err: false, message: '', result: rows});
 });
